7b6808d9a813d52630ba26f6e172e5760b078e8c
Alamsyah Bimasakti <alamsyahbima22@gmail.com>
2022-12-02 03:53:48 +0700

    add readme

---
+++ readme.md
@@ -0,0 +1,78 @@
+<div align="center">
+  <img src="./readme/logo.png" width="200px" height="200px" />
+</div>
+<h3 align="center">Blanja Frontend</h3>
+<p align="center">
+  <a href="https://blanja-frontend.vercel.app/">View Demo</a>
+</p>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+blanja is a website-based application that makes it easy for people to get products that are sold online by other people. blanja is developed with PERN satck (PostgreSQL, Express.js, React.js, Node.js). Blanja is an Individual project, here I created Backend and Frontend. blanja is a website-based application that makes it easy for people to get products that are sold online by other people. blanja is developed with PERN satck (PostgreSQL, Express js, Reactjs, Node js). blanja is an Individual project, here I create Backend and Frontend.
+
+### Technology Used
+
+- [ReactJS](https://reactjs.org/)
+- [Redux](https://redux.js.org/)
+- [Bootstrap](https://getbootstrap.com/)
+- [Axios](https://github.com/axios/axios)
+
+<!-- GETTING STARTED -->
+
+## Getting Started
+
+### Installation
+
+- Clone This Repository
+
+`git clone https://github.com/Massbim/blanja-frontend`
+
+- Install Module
+
+`npm install`
+
+- Setting .env
+
+```bash
+REACT_APP_API_BACKEND=YOUR_API_URL
+```
+
+### Executing program
+
+- Run this project with `npm run start`.
+
+<!-- SCREENSHOT -->
+
+## Screenshot
+
+### Register
+
+<img src="./readme/blanja-1.png" />
+
+### Login
+
+<img src="./readme/blanja-2.png" />
+
+### Home
+
+<img src="./readme/blanja-3.png" />
+
+### Product Detail
+
+<img src="./readme/blanja-5.png" />
+
+<!-- CONTACT INFO -->
+
+## Contact Info
+
+Contributors names and contact info:
+
+1. Alamsyah Bimasakti Qobus Nur Rahman
+
+- [Linkedin](https://www.linkedin.com/in/alamsyahbimasakti)
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE file for details
--- src/components/module/home/Detail/Content.js
+++ src/components/module/home/Detail/Content.js
@@ -2,32 +2,29 @@
 import React, { useEffect, useState } from "react";
 import "./StyleDetail.css";
 // import axios from "axios";
-import { useParams, Link,useNavigate } from "react-router-dom";
+import { useParams, Link, useNavigate } from "react-router-dom";
 import retanggle from "../../../../assets/image/detail products/Rectangle 21.png";
 import shape from "../../../../assets/image/detail products/Shape (1).png";
-import './StyleDetail.css'
+import "./StyleDetail.css";
 import { addMycart } from "../../../../configs/redux/actions/bagAction";
-import {FormatRupiah} from "@arismun/format-rupiah"
-import axios from "axios"
-
-
+import { FormatRupiah } from "@arismun/format-rupiah";
+import axios from "axios";
 
 const Content = () => {
-  const navigate = useNavigate()
-  const {id} = useParams()
+  const navigate = useNavigate();
+  const { id } = useParams();
   const [products, setProducts] = useState([]);
   useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_BACKEND}products/${id}`)
-      .then( (response)=> {
+      .then((response) => {
         setProducts(response.data.data[0]);
       })
-      .catch( (error)=> {
+      .catch((error) => {
         console.log(error);
       });
   }, []);
   const handleAddBag = async (detailProductId, navigate) => {
-
     const data = {
       productId: detailProductId,
       qty: 1,
@@ -35,20 +32,20 @@ const Content = () => {
     // dispatch()
     addMycart(data, navigate);
   };
-   const [count, setCount] = useState(1);
-   const handleSum = () => {
-     setCount(count + 1);
-   };
-   const handleMin = () => {
-     setCount(count - 1);
-   };
-   const [countSize, setCounts] = useState(1);
-   const handleSums = () => {
-     setCounts(countSize + 1);
-   };
-   const handleMins = () => {
-     setCounts(countSize - 1);
-   };
+  const [count, setCount] = useState(1);
+  const handleSum = () => {
+    setCount(count + 1);
+  };
+  const handleMin = () => {
+    setCount(count - 1);
+  };
+  const [countSize, setCounts] = useState(1);
+  const handleSums = () => {
+    setCounts(countSize + 1);
+  };
+  const handleMins = () => {
+    setCounts(countSize - 1);
+  };
   return (
     <div>
       {/* {(products.data).length === 0 ? (
@@ -57,138 +54,135 @@ const Content = () => {
           &nbsp;Loading
         </div>
       ) : ( */}
-        <div className="container child-page">
-          <div className="row mt-3">
-            <div className="col-lg-5">
-              <div className="galleries">
-                <div className="galleries-container">
+      <div className="container child-page">
+        <div className="row mt-3">
+          <div className="col-lg-5">
+            <div className="galleries">
+              <div className="galleries-container">
                 <img src={products.photo} className="w-100" alt="" />
-                </div>
-                <div className="thumb mt-3 text-center">
-                  <a
-                    href="../image/detail products/secondary1.png"
-                    className=""
-                  >
-                    <img src={products.photo} alt="" />
-                  </a>
-                  <a
-                    href="../image/detail products//secondary2.png"
-                    className="ms-2"
-                  >
-                    <img src={products.photo} alt="" />
-                  </a>
-                  <a
-                    href="../image/detail products/image2 (1).png"
-                    className="ms-2"
-                  >
-                    <img src={products.photo} alt="" />
-                  </a>
-                  <a
-                    href="../image/detail products/image2 (2).png"
-                    className="ms-2"
-                  >
-                    <img src={products.photo} alt="" />
-                  </a>
-                  <a
-                    href="../image/detail products/image2 (3).png"
-                    className="ms-2"
-                  >
-                    <img src={products.photo} alt="" />
-                  </a>
-                </div>
               </div>
-            </div>
-            <div className="col-lg-6  ms-2">
-              <div className="title-product">
-                <h3 className="product-title mt-3">{products.name}</h3>
-                <p className="text-secondary sub-title mt-3">{products.merk}</p>
-                <div className="d-flex justify-content-start text-warning start mt-5">
-                  <div className="bi-star-fill"></div>
-                  <div className="bi-star-fill"></div>
-                  <div className="bi-star-fill"></div>
-                  <div className="bi-star-fill"></div>
-                  <div className="bi-star-fill"></div>
-                  <p className="rating  mt-2 ms-1 text-dark">(10)</p>
-                </div>
-              </div>
-              <div className="price-products mt-2">
-                <p className="mb-3 title-price">Price</p>
-                <h3 className="price-detail mt-2">
-                  <FormatRupiah value={products.price} />
-                </h3>
-              </div>
-              <div className="color-products mt-4 ">
-                <p className="title-color">Color</p>
-                <div className="d-flex justify-content-start start ">
-                  <div className="d-flex  align-items-center btn-border">
-                    <button className="btn btn-black"></button>
-                  </div>
-                  <button className="btn btn-red"></button>
-                  <button className="btn btn-blue"></button>
-                  <button className="btn btn-green"></button>
-                </div>
-              </div>
-              <div className="size mt-3">
-                <div className="d-flex justify-content-start ms-1 text-black">
-                  <p className="title-size">Size</p>
-                  <p className="title-jumlah ms-5 text-black">Jumlah</p>
-                </div>
-                <div className="d-flex justify-content-start mt-1 ms-2">
-                  <div className="d-flex btn-min btn" onClick={handleMins}>
-                    <img src={retanggle} className="m-auto icon" alt="" />
-                  </div>
-                    <p className="ms-2 me-2 mt-1 size size1">{ countSize}</p>
-                  <div className="d-flex btn-max btn" onClick={handleSums}>
-                    <img src={shape} className="m-auto icon" alt="" />
-                  </div>
-                  <div className="d-flex btn-min btn ms-5" onClick={handleMin}>
-                    <img src={retanggle} className="m-auto icon" alt="" />
-                  </div>
-                  <p className="ms-2 me-2 mt-1 size size1">{count}</p>
-                  <div className="d-flex btn-max btn" onClick={handleSum}>
-                    <img src={shape} className="m-auto icon" alt="" />
-                  </div>
-                </div>
-              </div>
-              <div className="submit mt-5 mb-5">
-                <div className="d-flex justify-content-start  ms-1">
-                  <div className="col-lg-3 ">
-                    <button className="btn btn-chat btn-event">Chat</button>
-                  </div>
-                  <div className="col-lg-4 ms-1">
-                    <Link to="/Bag">
-                      <button
-                        className="btn btn-bag btn-event"
-                        onClick={() => {
-                          console.log(products.data.id);
-                          handleAddBag(products.data.id, navigate);
-                        }}
-                      >
-                        Add bag
-                      </button>
-                    </Link>
-                  </div>
-                  <div className="col-lg-5 ms-1">
-                    <Link to="/Checkout">
-                      <button className="btn btn-buy btn-event">Buy Now</button>
-                    </Link>
-                  </div>
-                </div>
+              <div className="thumb mt-3 text-center">
+                <a href="../image/detail products/secondary1.png" className="">
+                  <img src={products.photo} alt="" />
+                </a>
+                <a
+                  href="../image/detail products//secondary2.png"
+                  className="ms-2"
+                >
+                  <img src={products.photo} alt="" />
+                </a>
+                <a
+                  href="../image/detail products/image2 (1).png"
+                  className="ms-2"
+                >
+                  <img src={products.photo} alt="" />
+                </a>
+                <a
+                  href="../image/detail products/image2 (2).png"
+                  className="ms-2"
+                >
+                  <img src={products.photo} alt="" />
+                </a>
+                <a
+                  href="../image/detail products/image2 (3).png"
+                  className="ms-2"
+                >
+                  <img src={products.photo} alt="" />
+                </a>
               </div>
             </div>
           </div>
-          <div className="row mt-3">
-            <div className="col-lg-12">
-              <h3 className="title-info">Informasi Produk</h3>
-              <h4 className="mt-4 text-sub">Condition</h4>
-              <p className="text-danger new">{products.condition}</p>
-              <p className="text-sub text-sub">Description</p>
-              <div className="text ms-1 text-secondary">
-                <p>{products.description}</p>
+          <div className="col-lg-6  ms-2">
+            <div className="title-product">
+              <h3 className="product-title mt-3">{products.name}</h3>
+              <p className="text-secondary sub-title mt-3">{products.merk}</p>
+              <div className="d-flex justify-content-start text-warning start mt-5">
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+                <div className="bi-star-fill"></div>
+                <p className="rating  mt-2 ms-1 text-dark">(10)</p>
+              </div>
+            </div>
+            <div className="price-products mt-2">
+              <p className="mb-3 title-price">Price</p>
+              <h3 className="price-detail mt-2">
+                <FormatRupiah value={products.price} />
+              </h3>
+            </div>
+            <div className="color-products mt-4 ">
+              <p className="title-color">Color</p>
+              <div className="d-flex justify-content-start start ">
+                <div className="d-flex  align-items-center btn-border">
+                  <button className="btn btn-black"></button>
+                </div>
+                <button className="btn btn-red"></button>
+                <button className="btn btn-blue"></button>
+                <button className="btn btn-green"></button>
+              </div>
+            </div>
+            <div className="size mt-3">
+              <div className="d-flex justify-content-start ms-1 text-black">
+                <p className="title-size">Size</p>
+                <p className="title-jumlah ms-5 text-black">Jumlah</p>
+              </div>
+              <div className="d-flex justify-content-start mt-1 ms-2">
+                <div className="d-flex btn-min btn" onClick={handleMins}>
+                  <img src={retanggle} className="m-auto icon" alt="" />
+                </div>
+                <p className="ms-2 me-2 mt-1 size size1">{countSize}</p>
+                <div className="d-flex btn-max btn" onClick={handleSums}>
+                  <img src={shape} className="m-auto icon" alt="" />
+                </div>
+                <div className="d-flex btn-min btn ms-5" onClick={handleMin}>
+                  <img src={retanggle} className="m-auto icon" alt="" />
+                </div>
+                <p className="ms-2 me-2 mt-1 size size1">{count}</p>
+                <div className="d-flex btn-max btn" onClick={handleSum}>
+                  <img src={shape} className="m-auto icon" alt="" />
+                </div>
+              </div>
+            </div>
+            <div className="submit mt-5 mb-5">
+              <div className="d-flex justify-content-start  ms-1">
+                <div className="col-lg-3 ">
+                  <button className="btn btn-chat btn-event">Chat</button>
+                </div>
+                <div className="col-lg-4 ms-1">
+                  <Link to="/Bag">
+                    <button
+                      className="btn btn-bag btn-event"
+                      onClick={() => {
+                        console.log(products.data.id);
+                        handleAddBag(products.data.id, navigate);
+                      }}
+                    >
+                      Add bag
+                    </button>
+                  </Link>
+                </div>
+                <div className="col-lg-5 ms-1">
+                  <Link to="/Checkout">
+                    <button className="btn btn-buy btn-event">Buy Now</button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
+        <div className="row mt-3">
+          <div className="col-lg-12">
+            <h3 className="title-info">Informasi Produk</h3>
+            <h4 className="mt-4 text-sub">Condition</h4>
+            <p className="text-danger new">{products.condition}</p>
+            <p className="text-sub text-sub">Description</p>
+            <div className="text ms-1 text-secondary">
+              <p>{products.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* )} */}
     </div>
   );
